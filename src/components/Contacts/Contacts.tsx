95dfcc0c5fe6280754d95ae0aa301a80725d985b
yanayukhymovych <yuhimovich.yana@gmail.com>
2021-03-31 19:59:49 +0300

    Added favicon

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -35,6 +35,11 @@ const Contacts = () => {
     },
   ];
 
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
   return (
     <div className="contacts">
       <div className="contacts__wrapper">
